1935a98f61eef589644ddbaac3e8b170ba4ef687
Yusron Hanan <yusronzain@gmail.com>
2018-02-17 06:23:15 +0700

    Add Watering Service

---
+++ app/src/main/java/com/example/android/mygarden/PlantWateringService.java
@@ -0,0 +1,59 @@
+package com.example.android.mygarden;
+
+/**
+ * Created by kambi on 17/02/2018.
+ */
+
+import android.app.IntentService;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+
+import com.example.android.mygarden.provider.PlantContract;
+import com.example.android.mygarden.utils.PlantUtils;
+
+import static com.example.android.mygarden.provider.PlantContract.BASE_CONTENT_URI;
+import static com.example.android.mygarden.provider.PlantContract.PATH_PLANTS;
+
+/**
+ * Created by wahaz on 08/11/2017.
+ */
+public class PlantWateringService extends IntentService {
+
+    public static final String ACTION_WATER_PLANTS = "com.example.android.mygarden.action.water_plants";
+
+    public PlantWateringService() {
+        super("PlantWateringService");
+    }
+
+    public static void startActionWaterPlants(Context context) {
+        Intent intent = new Intent(context, PlantWateringService.class);
+        intent.setAction(ACTION_WATER_PLANTS);
+        context.startService(intent);
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        if (intent != null) {
+            final String action = intent.getAction();
+            if (ACTION_WATER_PLANTS.equals(action)) {
+                handleActionWaterPlants();
+            }
+        }
+    }
+
+    private void handleActionWaterPlants() {
+        Uri PLANTS_URI = BASE_CONTENT_URI.buildUpon().appendPath(PATH_PLANTS).build();
+        ContentValues contentValues = new ContentValues();
+        long timeNow = System.currentTimeMillis();
+        contentValues.put(PlantContract.PlantEntry.COLUMN_LAST_WATERED_TIME, timeNow);
+
+        getContentResolver().update(
+                PLANTS_URI,
+                contentValues,
+                PlantContract.PlantEntry.COLUMN_LAST_WATERED_TIME + ">?",
+                new String[]{String.valueOf(timeNow - PlantUtils.MAX_AGE_WITHOUT_WATER)}
+        );
+    }
+}
--- app/src/main/java/com/example/android/mygarden/provider/PlantWidgetProvider.java
+++ app/src/main/java/com/example/android/mygarden/provider/PlantWidgetProvider.java
@@ -11,15 +11,23 @@ import android.content.Context;
 import android.content.Intent;
 import android.widget.RemoteViews;
 
+import com.example.android.mygarden.PlantWateringService;
 import com.example.android.mygarden.R;
 import com.example.android.mygarden.ui.MainActivity;
 
+/**
+ * Created by wahaz on 08/11/2017.
+ */
 public class PlantWidgetProvider extends AppWidgetProvider {
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager, int appwidgetId) {
         Intent intent = new Intent(context, MainActivity.class);
         PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.plant_widget);
         views.setOnClickPendingIntent(R.id.widget_plant_image, pendingIntent);
+        Intent wateringIntent = new Intent(context, PlantWateringService.class);
+        wateringIntent.setAction(PlantWateringService.ACTION_WATER_PLANTS);
+        PendingIntent wateringPendingIntent = PendingIntent.getService(context, 0, wateringIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        views.setOnClickPendingIntent(R.id.widget_water_button, wateringPendingIntent);
         appWidgetManager.updateAppWidget(appwidgetId, views);
     }
 
